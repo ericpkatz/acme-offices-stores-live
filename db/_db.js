a9a42e5bb6720c5fcb31d9db4b1664759b7d8217
Eric P. Katz <ericpkatz@gmail.com>
2017-02-27 16:32:53 -0500

    better error

--- db/_db.js
+++ db/_db.js
@@ -1,5 +1,10 @@
 const Sequelize = require('sequelize');
 
+if(!process.env.DATABASE_URL){
+  throw 'No environment variable for database-- process.env.DATABASE_URL';
+}
+
 const db = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = db;
+
